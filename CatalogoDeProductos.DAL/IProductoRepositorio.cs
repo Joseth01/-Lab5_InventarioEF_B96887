5d28e776bd2c5f091c94ac03e298a30bd3ae529e
JOSETH.ROJAS@ucr.ac.cr <JOSETH.ROJAS@ucr.ac.cr>
2026-04-28 15:40:31 -0600

    Creacion de interface IProductoRepositorio y clase ProductoRepositorio en el proyecto .DAL

---
+++ CatalogoDeProductos.DAL/IProductoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using CatalogoDeProductos.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogoDeProductos.DAL
+{
+    public interface IProductoRepositorio
+    {
+        List<Producto> ObtengaTodosLosProductos();
+        Producto ObtengaElProductoPorId(int id);
+        void AgregueElProducto(Producto producto);
+        void ActualiceElProducto(Producto producto);
+        void ElimineElProducto(int id);
+    }
+}
---
+++ CatalogoDeProductos.DAL/ProductoRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using CatalogoDeProductos.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatalogoDeProductos.DAL
+{
+    public class ProductoRepositorio : IProductoRepositorio
+    {
+        private readonly AppDbContext _contexto;
+
+        public ProductoRepositorio(AppDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public List<Producto> ObtengaTodosLosProductos()
+        {
+            return _contexto.Productos.ToList();
+        }
+
+        public Producto ObtengaElProductoPorId(int id)
+        {
+            return _contexto.Productos.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void AgregueElProducto(Producto producto)
+        {
+            _contexto.Productos.Add(producto);
+            _contexto.SaveChanges();
+        }
+
+        public void ActualiceElProducto(Producto producto)
+        {
+            _contexto.Productos.Update(producto);
+            _contexto.SaveChanges();
+        }
+
+        public void ElimineElProducto(int id)
+        {
+            var producto = ObtengaElProductoPorId(id);
+            if (producto != null)
+            {
+                _contexto.Productos.Remove(producto);
+                _contexto.SaveChanges();
+            }
+        }
+    }
+}
